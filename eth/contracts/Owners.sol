a9c10dc88fa70293d208d3f55f2bfd27e58dc7df
yakkomajuri <38760734+yakkomajuri@users.noreply.github.com>
2019-03-11 20:15:07 +0000

    Update Owners.sol

--- eth/contracts/Owners.sol
+++ eth/contracts/Owners.sol
@@ -1,13 +1,12 @@
 pragma solidity^0.5.0;
 
 contract Owners {
-
-    modifier onlyOwners() {
+    
+    modifier onlyOwners() {  
         require(isOwner[msg.sender]);
         _;
     }
-
-    Owners ownerContract;
+    
 
     constructor(address _owner1, address _owner2) public {
         isOwner[msg.sender] = true;
@@ -21,27 +20,41 @@ contract Owners {
     mapping(address => mapping(address => bool)) hasVotedToRemove;
     mapping(address => uint8) public votesToAdd;
     mapping(address => uint8) public votesToRemove;
+    
+    address[] public addressesVotedOn;
 
     uint8 public numberOfVoters;
-
+    
     function voteToAdd(address _ad) public onlyOwners {
         require(hasVotedToAdd[msg.sender][_ad] == false,
         "this");
         require(isOwner[_ad] == false,
         "that");
         hasVotedToAdd[msg.sender][_ad] = true;
+        if (votesToAdd[_ad] == uint8(0)) {
+            addressesVotedOn.push(_ad);
+        if (addressesVotedOn.length == 15) {
+                reset();
+            }
+        }
         votesToAdd[_ad]++;
         enoughVotesToAdd(_ad);
     }
-
+    
     function voteToRemove(address _ad) public onlyOwners {
         require(hasVotedToRemove[msg.sender][_ad] == false);
         require(isOwner[_ad]);
         hasVotedToRemove[msg.sender][_ad] = true;
+        if (votesToRemove[_ad] == uint8(0)) {
+            addressesVotedOn.push(_ad);
+        if (addressesVotedOn.length == 15) {
+                reset();
+            }
+        }
         votesToRemove[_ad]++;
         enoughVotesToRemove(_ad);
-    }
-
+    } 
+    
     function removeMyVote(address _ad, uint8 _choice) public onlyOwners {
         if (_choice == 0) {
             require(hasVotedToAdd[msg.sender][_ad]);
@@ -54,7 +67,7 @@ contract Owners {
             votesToRemove[_ad]--;
         }
     }
-
+    
     function enoughVotesToAdd(address _ad) internal {
         if (votesToAdd[_ad] * 2 > numberOfVoters) {
             numberOfVoters += 1;
@@ -62,17 +75,40 @@ contract Owners {
         }
         else { }
     }
-
+    
     function enoughVotesToRemove(address _ad) internal {
         if (votesToRemove[_ad] * 2 > numberOfVoters) {
             numberOfVoters -= 1;
             isOwner[_ad] = false;
+            removeOwnerVotes(_ad);
         }
         else { }
     }
-
+    
     function getOwners(address _ad) external view returns(bool) {
         return isOwner[_ad];
     }
-
+    
+    
+    function reset() internal {
+        for (uint i = 0; i < addressesVotedOn.length; i++) {
+            votesToAdd[addressesVotedOn[i]] = 0;
+            votesToRemove[addressesVotedOn[i]] = 0;
+            delete addressesVotedOn[i];
+        }
+    }
+    
+    function removeOwnerVotes(address _ad) internal {
+        address a;
+        for (uint i = 0; i < addressesVotedOn.length; i++) {
+            a = addressesVotedOn[i];
+            if (hasVotedToRemove[_ad][a]) {
+                votesToRemove[a]--;
+            }
+            if (hasVotedToAdd[_ad][a]){
+                votesToAdd[a]--;
+            }
+        }
+    }
+    
 }
